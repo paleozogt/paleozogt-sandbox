535dd079856a69dfcf94447b8787b1bf2525a021
asimmons <asimmons@ab3448a9-6bf0-d6da-fa9e-c7fe7c7ecd46>
2010-10-01 21:01:53 +0000

    making the game only take 200 turns

--- galcon/run-game.sh
+++ galcon/run-game.sh
@@ -4,4 +4,4 @@ bot1=$1
 bot2=$2
 map=$3
 
-java -jar tools/PlayGame.jar maps/map${map}.txt 1000 1000 log.txt "java -jar dist/$bot1.jar" "java -jar dist/$bot2.jar" | java -jar tools/ShowGame.jar
+java -jar tools/PlayGame.jar maps/map${map}.txt 1000 200 log.txt "java -jar dist/$bot1.jar" "java -jar dist/$bot2.jar" | java -jar tools/ShowGame.jar
