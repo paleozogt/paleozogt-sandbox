589e95c7ab2a486158fbd972edd9f845166de24d
asimmons <asimmons@ab3448a9-6bf0-d6da-fa9e-c7fe7c7ecd46>
2010-10-01 19:03:14 +0000

    code formatting

--- galcon/src/Planet.java
+++ galcon/src/Planet.java
@@ -1,59 +1,55 @@
 public class Planet implements Cloneable {
     // Initializes a planet.
-    public Planet(int planetID,
-                  int owner,
-		  int numShips,
-		  int growthRate,
-		  double x,
-		  double y) {
-	this.planetID = planetID;
-	this.owner = owner;
-	this.numShips = numShips;
-	this.growthRate = growthRate;
-	this.x = x;
-	this.y = y;
+    public Planet(int planetID, int owner, int numShips, int growthRate,
+            double x, double y) {
+        this.planetID = planetID;
+        this.owner = owner;
+        this.numShips = numShips;
+        this.growthRate = growthRate;
+        this.x = x;
+        this.y = y;
     }
 
     // Accessors and simple modification functions. These should be mostly
     // self-explanatory.
     public int PlanetID() {
-	return planetID;
+        return planetID;
     }
 
     public int Owner() {
-	return owner;
+        return owner;
     }
 
     public int NumShips() {
-	return numShips;
+        return numShips;
     }
 
     public int GrowthRate() {
-	return growthRate;
+        return growthRate;
     }
 
     public double X() {
-	return x;
+        return x;
     }
 
     public double Y() {
-	return y;
+        return y;
     }
 
     public void Owner(int newOwner) {
-	this.owner = newOwner;
+        this.owner = newOwner;
     }
 
     public void NumShips(int newNumShips) {
-	this.numShips = newNumShips;
+        this.numShips = newNumShips;
     }
 
     public void AddShips(int amount) {
-	numShips += amount;
+        numShips += amount;
     }
 
     public void RemoveShips(int amount) {
-	numShips -= amount;
+        numShips -= amount;
     }
 
     private int planetID;
@@ -62,16 +58,16 @@ public class Planet implements Cloneable {
     private int growthRate;
     private double x, y;
 
-    private Planet (Planet _p) {
-	planetID = _p.planetID;
-	owner = _p.owner;
-	numShips = _p.numShips;
-	growthRate = _p.growthRate;
-	x = _p.x;
-	y = _p.y;
+    private Planet(Planet _p) {
+        planetID = _p.planetID;
+        owner = _p.owner;
+        numShips = _p.numShips;
+        growthRate = _p.growthRate;
+        x = _p.x;
+        y = _p.y;
     }
 
     public Object clone() {
-	return new Planet(this);
+        return new Planet(this);
     }
 }
