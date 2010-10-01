3e18414a5004ce643b10b49e1a37de9243de0085
asimmons <asimmons@ab3448a9-6bf0-d6da-fa9e-c7fe7c7ecd46>
2010-10-01 19:45:06 +0000

    changing run-game script to allow for specifying map

--- galcon/run-game.sh
+++ galcon/run-game.sh
@@ -2,6 +2,6 @@
 
 bot1=$1
 bot2=$2
-map=map7.txt
+map=$3
 
-java -jar tools/PlayGame.jar maps/$map 1000 1000 log.txt "java -jar dist/$bot1.jar" "java -jar dist/$bot2.jar" | java -jar tools/ShowGame.jar
+java -jar tools/PlayGame.jar maps/map${map}.txt 1000 1000 log.txt "java -jar dist/$bot1.jar" "java -jar dist/$bot2.jar" | java -jar tools/ShowGame.jar
