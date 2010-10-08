ee628bf5b41947fbd1aa8145ef4aec463dec0ee2
paleozogt <paleozogt@ab3448a9-6bf0-d6da-fa9e-c7fe7c7ecd46>
2010-10-08 21:34:05 +0000

    refactoring run script

--- galcon/run-game.sh
+++ galcon/run-game.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-bot1=$1
-bot2=$2
+bot1=dist/$1.jar
+bot2=dist/$2.jar
 map=$3
 
-java -jar tools/PlayGame.jar maps/map${map}.txt 1000 200 log.txt "java -jar dist/$bot1.jar" "java -jar dist/$bot2.jar" | java -jar tools/ShowGame.jar
+echo "$bot1 vs $bot2"
+java -jar tools/PlayGame.jar maps/map${map}.txt 1000 200 log.txt "java -jar $bot1" "java -jar $bot2" | java -jar tools/ShowGame.jar
